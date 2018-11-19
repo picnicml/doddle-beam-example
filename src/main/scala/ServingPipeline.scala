fe06151905b96dc3d0b7f15e59dcfb528ac32e86
inejc <nejc.ilenic@gmail.com>
2018-11-19 12:47:18 +0100

    use a composite trigger

--- src/main/scala/ServingPipeline.scala
+++ src/main/scala/ServingPipeline.scala
@@ -1,13 +1,17 @@
 import breeze.linalg.DenseMatrix
 import com.picnicml.doddlemodel.data.{Dataset, Simplex, Target, loadBreastCancerDataset}
 import com.picnicml.doddlemodel.linear.LogisticRegression
+import com.picnicml.doddlemodel.syntax.ClassifierSyntax._
 import com.spotify.scio.ContextAndArgs
 import com.spotify.scio.values.WindowOptions
-import org.apache.beam.sdk.transforms.windowing.{AfterProcessingTime, Repeatedly}
+import org.apache.beam.sdk.transforms.windowing.{AfterFirst, AfterPane, AfterProcessingTime, Repeatedly}
 import org.apache.beam.sdk.values.WindowingStrategy.AccumulationMode
 import org.joda.time.Duration
 
 object ServingPipeline {
+
+  private val minBatchElements = 10
+  private val maxBatchWaitTimeInMillis = 50
 
   // e.g. replace this with loading a pretrained model from a GCS bucket
   val (x, y) = loadBreastCancerDataset
@@ -16,15 +20,19 @@ object ServingPipeline {
   def main(args: Array[String]): Unit = {
     val (scioContext, _) = ContextAndArgs(args)
 
+    // a composite trigger that fires whenever the pane has at least 'minBatchElements' elements
+    // or after 'maxBatchWaitTimeInMillis', whatever happens first
+    val groupedWithinTrigger = Repeatedly.forever(AfterFirst.of(
+      AfterPane.elementCountAtLeast(minBatchElements),
+      AfterProcessingTime.pastFirstElementInPane().plusDelayOf(Duration.millis(maxBatchWaitTimeInMillis))
+    ))
+
     scioContext
       // e.g. replace this with a pubsub source to turn the pipeline into a streaming job
       .textFile(getClass.getResource("breast_cancer.csv").toString)
 
-      // put all examples that arrive to the pipeline within the same 10 millisecond interval into
-      // a batch (note that intervals are non-overlapping), i.e. emit data every 10 milliseconds,
-      // triggering should be tuned for optimal throughput
       .withGlobalWindow(WindowOptions(
-        trigger = Repeatedly.forever(AfterProcessingTime.pastFirstElementInPane().plusDelayOf(Duration.millis(10))),
+        trigger = groupedWithinTrigger,
         accumulationMode = AccumulationMode.DISCARDING_FIRED_PANES,
         allowedLateness = Duration.ZERO))
 
